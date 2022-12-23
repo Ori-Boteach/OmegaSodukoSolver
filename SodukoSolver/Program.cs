bce475944ed1252dd20b73c7a13a4490a25df79c
Ori-Boteach <orib240@gmai.com>
2022-12-23 16:59:04 +0200

    divided to initial moduls

--- SodukoSolver/Program.cs
+++ SodukoSolver/Program.cs
@@ -2,11 +2,12 @@
 
 namespace SodukoSolver
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Now: my first commit to dev branch");
+            Console.WriteLine("Now: my first commit to dev branch"); // This is my first commit to dev branch
         }
     }
+    
 }
