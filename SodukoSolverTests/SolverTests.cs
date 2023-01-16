68d9b8a6198847a3cc7b08feb4832154b57a9714
Ori-Boteach <orib240@gmai.com>
2023-01-16 19:37:36 +0200

    FINISHED MY PROJECT!!!

--- SodukoSolverTests/SolverTests.cs
+++ SodukoSolverTests/SolverTests.cs
@@ -138,5 +138,68 @@ namespace SodukoSolverTests
             // Assert
             Assert.That(actualSolve, Is.EqualTo(expectedSolve));
         }
+        [Test]
+        public void ValidationAndStart_Test_Invalid_1() // testing an invalid input of soduko puzzle -> throwing InvalidInputPlaceException for coliding numbers in the same row / column or cube
+        {
+            // Act and assert
+            Assert.Throws<InvalidInputPlaceException>(() => _ui.ValidationAndStart("000005080000601043000000000010500000000106000300000005530000061000000004000000077"));
+        }
+
+        [Test]
+        public void ValidationAndStart_Test_Invalid_2() // testing an invalid input of soduko puzzle -> throwing InvalidInputPlaceException for coliding numbers in the same row / column or cube
+        {
+            // Act and assert
+            Assert.Throws<InvalidInputPlaceException>(() => _ui.ValidationAndStart("259867000000000000000300085040023050970580000038046071487635102091200068025008433"));
+        }
+
+        [Test]
+        public void ValidationAndStart_InputExceptionsTest_1() // testing exceptions being raised because of varius invalid sodukos
+        {
+            // Act and assert
+            Assert.Throws<InvalidInputCharException>(() => _ui.ValidationAndStart("22")); // -> throwing InvalidInputCharException in case an invalid char is used for a certain sudoku size 
+        }
+
+        [Test]
+        public void ValidationAndStart_InputExceptionsTest_2() // testing exceptions being raised because of varius invalid sodukos
+        {
+            // Act and assert
+            Assert.Throws<InvalidInputLengthException>(() => _ui.ValidationAndStart("")); // -> throwing InvalidInputLengthException for an invalid size (not: 1/4/9/16/25)
+        }
+
+        [Test]
+        public void GetInputAsFile_FileExceptionsTest2() // testing response to file input which is not type .txt
+        {
+            // Arrange
+            string input = "C:\\Users\\user\\Downloads\\not_on_pc.pptx";
+            StringReader stringReader = new(input);
+            Console.SetIn(stringReader);
+
+            Assert.Throws<InvalidFileTypeException>(() => _ui.GetInputAsFile()); // -> throwing InvalidFileTypeException
+        }
+
+        [Test]
+        public void GetInputAsFile_FileExceptionsTest() // testing response to file input
+        {
+            // For a non existing file:
+            // Arrange
+            string input = "C:\\Users\\user\\Downloads\\not_on_pc.txt";
+            StringReader stringReader = new(input);
+            Console.SetIn(stringReader);
+
+            string expectedOutput = "\nEnter the file path:\r\n\nError: File not found -> file path should be like C:\\Users\\user\\Downloads\\sudoku_example.txt\r\n";
+            StringWriter stringWriter = new();
+            Console.SetOut(stringWriter);
+
+            // for a non txt file:
+
+            // Act
+            _ui.GetInputAsFile();
+            string actualOutput = stringWriter.ToString();
+
+            // Assert
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+
+            // For a valid file -> same result as being inputted as a string
+        }
     }
 }
